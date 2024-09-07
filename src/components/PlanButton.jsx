7576047a586ebcc171550b703cb20afb66563809
OsatohanmwenT <143819001+OsatohanmwenT@users.noreply.github.com>
2024-09-07 15:00:23 +0100

    Update PlanButton.jsx

--- src/components/PlanButton.jsx
+++ src/components/PlanButton.jsx
@@ -2,7 +2,7 @@
 
 export default function PlanButton({ price, handlePlanSelection, presentPlan, ...plan }) {
   return (
-  <button onClick={() => handlePlanSelection(plan.name)} className={`border-[1px] transition-all ${presentPlan === plan.name ? "bg-Magnolia border-Purplish-blue" : "" } flex-1 flex md:flex-col hover:border-Purplish-blue gap-3 rounded-lg p-4 border-Light-gray`}>
+  <button onClick={() => handlePlanSelection(plan.name)} className={`border-[1px] transition-all ${presentPlan === plan.name ? "bg-Magnolia border-Purplish-blue" : "" } flex-1 flex md:flex-col hover:border-Purplish-blue max-md:gap-2 gap-3 rounded-lg max-md:py-2 p-4 border-Light-gray`}>
     <img src={plan.image} alt={plan.alt} />
     <div className='flex flex-col items-start'>
       <p className='font-semibold text-Marine-blue text-lg'>{plan.name}</p>
